a44977694e8c618fb26a4ce53f626cd5b1e00b99
Ahmed Hany <a7madhany2003@gmail.com>
2024-09-25 19:11:20 +0300

    finished the chatbox component

--- front-end/src/components/ChatBox.svelte
+++ front-end/src/components/ChatBox.svelte
@@ -1,10 +1,100 @@
 <script>
+  import { openDataBase, getObjectStore, getAllRecordsIndex } from "../db/operations";
+  export let activeFriendVerificationPublicKey;
 
+  let messages = [];
+  let previousMessageTimestamp = null;
+  let chatboxElement;
+
+const chatUpdateChannel = new BroadcastChannel("chat_update");
+chatUpdateChannel.onmessage = async () => await getMessages(activeFriendVerificationPublicKey);
+
+async function getMessages(activeFriendVerificationPublicKey) {
+  if (!activeFriendVerificationPublicKey) {
+    return;
+  }
+  previousMessageTimestamp = null;
+
+  const db = await openDataBase("BlindLink", 1);
+  const objectStore = getObjectStore(db, "messages", "readonly");
+  const ourMessages = await getAllRecordsIndex(objectStore, "verification_public_key",activeFriendVerificationPublicKey);
+
+  // no need to sort, as messages are sorted by id, which is automatically incremented in the back-end
+
+  messages = ourMessages;
+  scrollToBottom();
+}
+
+function scrollToBottom() {
+  setTimeout(() => {
+    chatboxElement.scrollTop = chatboxElement.scrollHeight;
+  }, 0);
+}
+
+function formatDate(timestamp) {
+  const date = new Date(timestamp);
+  const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${date.getMonth().toString().padStart(2, '0')}/${date.getFullYear()}`
+  return formattedDate;
+}
+
+function formatTime(timestamp) {
+  const date = new Date(timestamp);
+  let hours = date.getHours();
+  let am_pm = hours >= 12? "pm" : "am";
+  hours = hours%12;
+  hours = hours? hours : 12;
+
+
+  const formattedDate = `${hours}:${date.getMinutes().toString().padStart(2, '0')} ${am_pm}`
+  return formattedDate; 
+}
+
+function isFirstMessageInDay(timestamp) {
+  if (previousMessageTimestamp === null) {
+    previousMessageTimestamp = timestamp;
+    return true;
+  }
+
+  const messageDate = new Date(timestamp);
+  const previousMessageDate = new Date(previousMessageTimestamp);
+
+  if (messageDate.getDate() > previousMessageDate.getDate()) {
+    previousMessageTimestamp = timestamp;
+    return true
+  }
+
+  previousMessageTimestamp = timestamp;
+  return false;
+}
+
+function daysSinceSent(timestamp) {
+  const date = new Date(timestamp); 
+  const currentDate = new Date(Date.now());
+
+  return currentDate.getDate() - date.getDate();
+}
+
+$: getMessages(activeFriendVerificationPublicKey);
 </script>
 
 
-<div class="chatbox">
-    <div class="message my_msg">
+<div class="chatbox" bind:this={chatboxElement}>
+  {#each messages as message}
+    {#if isFirstMessageInDay(message.timestamp)}
+      <div class="date"> {daysSinceSent(message.timestamp) > 1? formatDate(message.timestamp): (daysSinceSent(message.timestamp) === 0? "Today": "Yesterday")} </div>
+    {/if}
+
+    <div class="message {message.messageType === "sent"? "my_msg" : "friend_msg"}">
+      <p>
+        {message.message}
+        <br /> <span>
+          {formatTime(message.timestamp)}
+        </span>
+      </p>
+    </div>
+  {/each}
+
+    <!-- <div class="message my_msg">
       <p>Hi <br /><span>12:18</span></p>
     </div>
     <div class="message friend_msg">
@@ -104,7 +194,7 @@
           >12:15</span
         >
       </p>
-    </div>
+    </div> -->
   </div>
 
 
@@ -112,7 +202,8 @@
     .chatbox {
         position: relative;
         height: calc(100% - 150px);
-        padding: 50px;
+        padding-left: 50px;
+        padding-right: 50px;
         overflow-y: auto;
         scrollbar-color: #c2bebe #f1f1f1; /* thumb color, track color */
         scrollbar-width:auto; /* scrollbar thickness: auto, thin, none */
@@ -188,4 +279,16 @@
         );
     }
 
+    .date {
+      display: inline-block;
+      text-align: center;
+      margin: 20px auto;
+      padding: 5px 15px;
+      font-size: 14px;
+      color: #555; /* Slightly darker grey for text */
+      background-color: #e1e1e1; /* Light grey background for the block */
+      border-radius: 12px; /* Rounded corners for the block */
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15); /* Subtle shadow for depth */
+      font-weight: bold;
+    }
   </style>
