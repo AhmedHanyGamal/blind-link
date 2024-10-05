ddd23232daec10b3b914546fef68e31a93a7d762
Ahmed Hany <a7madhany2003@gmail.com>
2024-10-05 11:27:47 +0300

    put meaningful comments and removed meaningless ones

--- front-end/src/db/db.js
+++ front-end/src/db/db.js
@@ -12,21 +12,13 @@ import {
   addIndexedDBEntry,
 } from "./operations";
 
-// let db;
-
-// async function main() {
 async function initialize_DB() {
-  // if (db) {
-  //   db.close();
-  //   console.log("Closed existing DB connection before initializing a new one");
-  // }
-
   const request = indexedDB.open("BlindLink", 1);
   request.onupgradeneeded = async function (event) {
-    // console.log("in log owo");
-
     const db = event.target.result;
 
+    // messages objectStore structure:
+    // {id (the key), message, messageType (either "sent" or "received"), timestamp (epoch time in milliseconds), verification_public_key (index)}
     if (!db.objectStoreNames.contains("messages")) {
       const objectStore = db.createObjectStore("messages", {
         keyPath: "id",
@@ -38,6 +30,8 @@ async function initialize_DB() {
       );
     }
 
+    // contacts objectStore structure:
+    // {id (the key), contact_name, friend_status (at this point, either pending or friend), encryption_public_key, verification_public_key (index)}
     if (!db.objectStoreNames.contains("contacts")) {
       const objectStore = db.createObjectStore("contacts", {
         keyPath: "id",
@@ -50,97 +44,24 @@ async function initialize_DB() {
       );
     }
 
+    // myKeys objectStore structure:
+    // {id, decryption_private_key, encryption_public_key, signature_private_key, verification_public_key}
     if (!db.objectStoreNames.contains("myKeys")) {
       db.createObjectStore("myKeys", { keyPath: "id" });
     }
 
+    // friendRequests objectStore structure:
+    // {message_id (the key), friendRequester, intro_message, encryption_public_key, verification_public_key}
     if (!db.objectStoreNames.contains("friendRequests")) {
       db.createObjectStore("friendRequests", {
         keyPath: "message_id",
       });
     }
-
-    // console.log("working as expected UWU");
-
-    // let db = openDataBase("BlindLink", 1);
-    // await initialize_keys(db);
-    // const openRequest = indexedDB.open("BlindLink", 1);
-
-    // openRequest.onerror = (event) => {
-    //   console.error(
-    //     "couldn't open the newly initialized database to enter data. error code:",
-    //     event.target.errorCode
-    //   );
-    // };
-
-    // openRequest.onsuccess = async function (event) {
-    //   const inputDB = event.target.result;
-
-    //   const tx = db.transaction("myKeys", "readwrite");
-    //   const myKeysStore = tx.objectStore("myKeys");
-
-    //   const encryptionKeyPair = await generate_encryption_key_pair();
-    //   const signatureKeyPair = await generate_signature_key_pair();
-
-    //   const encryptionPublicKeyBase64 = await public_key_to_base64(
-    //     encryptionKeyPair.publicKey
-    //   );
-    //   const verificationPublicKeyBase64 = await public_key_to_base64(
-    //     signatureKeyPair.publicKey
-    //   );
-
-    //   const keys = {
-    //     id: 1,
-    //     encryption_public_key: encryptionPublicKeyBase64,
-    //     decryption_private_key: encryptionKeyPair.privateKey,
-    //     signature_private_key: signatureKeyPair.privateKey,
-    //     verification_public_key: verificationPublicKeyBase64,
-    //   };
-
-    //   const addRequest = myKeysStore.add(keys);
-
-    //   addRequest.onsuccess = () => {
-    //     console.log("indexedDB fully initialized successfully");
-    //   };
-
-    //   addRequest.onerror = (event) => {
-    //     console.error(
-    //       "error adding your keys to the database. error:",
-    //       event.target.errorCode
-    //     );
-    //   };
-    // };
-
-    // encryption_public_key
-    // verification_public_key
-    // intro_message
-
-    // messages.createIndex("message", "message", { unique: false });
-    // messages.createIndex("timestamp", "timestamp", { unique: false });
-    // messages.createIndex("contact_name", "contact_name", { unique: false }); //{unique: false} could do this
-
-    // contacts.createIndex("friend_status", "friend_status", {unique: false}); // either "pending" if the friend request wasn't accepted or "friend" if it was accepted
-    // contacts.createIndex("contact_name", "contact_name", { unique: false });
-    // contacts.createIndex("encryption_public_key", "encryption_public_key", {
-    //   unique: true,
-    // });
-    // contacts.createIndex("verification_public_key", "verification_public_key", {
-    //   unique: true,
-    // });
-
-    // myKeys.createIndex("encryption_public_key", "encryption_public_key");
-    // myKeys.createIndex("decryption_private_key", "decryption_private_key");
-    // myKeys.createIndex("signature_private_key", "signature_private_key");
-    // myKeys.createIndex("verification_public_key", "verification_public_key");
   };
 
   request.onsuccess = async function (event) {
     const db = event.target.result;
-
-    // console.log("DB:", db);
-
     const myKeysStore = getObjectStore(db, "myKeys", "readwrite");
-
     const firstRecord = await getFirstRecord(myKeysStore);
 
     if (!firstRecord) {
@@ -174,41 +95,6 @@ async function initialize_DB() {
   request.onerror = function (event) {
     console.error("Database error:", event.target.errorCode);
   };
-  // }
 }
 
-// async function initialize_keys(db) {
-//   const myKeysStore = getObjectStore(db, "myKeys", "readwrite");
-
-//   const firstRecord = await getFirstRecord(myKeysStore);
-
-//   if (!firstRecord) {
-//     const encryptionKeyPair = await generate_encryption_key_pair();
-//     const signatureKeyPair = await generate_signature_key_pair();
-
-//     const encryptionPublicKeyBase64 = await public_key_to_base64(
-//       encryptionKeyPair.publicKey
-//     );
-//     const verificationPublicKeyBase64 = await public_key_to_base64(
-//       signatureKeyPair.publicKey
-//     );
-
-//     const userKeys = {
-//       id: 1,
-//       encryption_public_key: encryptionPublicKeyBase64,
-//       decryption_private_key: encryptionKeyPair.privateKey,
-//       signature_private_key: signatureKeyPair.privateKey,
-//       verification_public_key: verificationPublicKeyBase64,
-//     };
-
-//     const myKeysStore2 = getObjectStore(db, "myKeys", "readwrite");
-//     addIndexedDBEntry(myKeysStore2, userKeys);
-//     console.log("Welcome new user, happy to have you with us :D");
-//   } else {
-//     console.log("Welcome back user :3");
-//   }
-// }
 export { initialize_DB };
-// export { main };
-
-// main();
